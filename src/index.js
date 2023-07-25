1649d4f22fdb89752badbabe4ac6007b5a6efe01
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-25 21:25:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
-import './index.css';
 import App from 'components/App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
