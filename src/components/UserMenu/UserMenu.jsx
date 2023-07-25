e2503df447f5a06cba91b510fb6ad1b0205eb1bc
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-25 21:25:35 +0300

    Create UserMenu.jsx

---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,12 @@
+
+
+const UserMenu = () => {
+    return (
+        <div>
+            <p>Hello, mango@mail.com</p>
+            <button type="button">Logout</button>
+        </div>
+    )
+}
+
+export default UserMenu;
