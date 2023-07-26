cd003466f1df321abe9b2dbd27ec90ebdb2a0ead
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-26 15:45:42 +0300

    Update contactsOperations.js

--- src/redux/contactsOperations.js
+++ src/redux/contactsOperations.js
@@ -1,35 +1,45 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = "https://64b5cb14f3dbab5a95c7aab9.mockapi.io";
+axios.defaults.headers.baseURL = 'http://connections-api.herokuapp.com';
 
 
-export const fetchContacts = createAsyncThunk('contacts/fetchContacts', async (_, thunkAPI) => {
-  try {
-    const response = await axios.get('/contacts');
+export const fetchContacts = createAsyncThunk('contacts/fetchContacts',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
 
-    return response.data;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.message);
-  }
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
 });
 
-export const addContact = createAsyncThunk('contacts/addContact', async (newContact, thunkAPI) => {
-  try {
-    const response = await axios.post('/contacts', newContact);
+export const addContact = createAsyncThunk('contacts/addContact',
+  async (newContact, thunkAPI) => {
+    try {
+      const response = await axios.post('/contacts', newContact);
 
-    return response.data;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.message);
-  }
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
 });
 
-export const removeContact = createAsyncThunk('contacts/removeContact', async (id, thunkAPI) => {
-  try {
-    const response =  await axios.delete(`contacts/${id}`);
+export const removeContact = createAsyncThunk('contacts/removeContact',
+  async(contactId, thunkAPI) => {
+    try {
+      const response =  await axios.delete(`/contacts/${contactId}`);
 
-    return response.data;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.message);
-  }
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
 });
+
+// export const updateContact = createAsyncThunk(
+//   'contacts/updateContact',
+//   async(contactId, credentials, thunkAPI) => {
+
+//   }
+// )
