68b84dc8fd78e728f37f53b500d4aaea510def0e
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-27 02:20:19 +0300

    Update contactsOperations.js

--- src/redux/contactsOperations.js
+++ src/redux/contactsOperations.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.headers.baseURL = 'http://connections-api.herokuapp.com';
+axios.defaults.headers.baseURL = 'https://connections-api.herokuapp.com';
 
 
 export const fetchContacts = createAsyncThunk('contacts/fetchContacts',
