2bb3c8d0ef9fa194d163803f9f803dd5db737f4a
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-26 15:44:03 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,50 @@
-import React from "react";
-import ContactList from "./ContactList";
-import FilterContacts from "./FilterContacts";
-import FormContact from "./FormContact";
-import { Toaster } from 'react-hot-toast';
-import { Container } from './App.styled';
+import { useAuth } from "hooks";
+import { useEffect, lazy} from "react";
+import { useDispatch } from "react-redux";
+import { Routes, Route } from "react-router-dom";
+import { refreshUser } from "redux/auth/authOperations";
+import { PrivateRoute } from "route/PrivateRout";
+import { RestrictedRoute } from "route/RestrictedRoute";
+import { Layout } from "./Layout";
+import Loader from "./Loader";
 
 
-export default function App() {
-  return (
-    <Container>
-      <h1>Phonebook</h1>
-      <FormContact />
-      <h2>Contacts</h2>
-      <FilterContacts />
-      <ContactList />
-      <Toaster />
-    </Container>
+const HomePage = lazy('../page/Home/Home');
+const RegisterPage = lazy('../page/RegisterPage/RegisterPage');
+const Login = lazy('../page/Login/Login');
+const Phonebook = lazy('../page/Phonebook/Phonebook');
+
+
+const App =() => {
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth;
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <Loader />
+  ) : (
+    <Routes>
+      <Route path="/" element={<Layout />} >
+        <Route index element={<HomePage />} />
+        <Route 
+        path="/register" 
+        element={<RestrictedRoute redirectTo="/contacts" component={<RegisterPage />} />} 
+        />
+        <Route 
+        path="/login"
+        element={
+          <RestrictedRoute redirectTo="/contacts" component={<Login />} />
+        } />
+        <Route
+        path="/contacts" 
+        element={<PrivateRoute redirectTo="/login" component={<Phonebook />} /> }
+        />
+      </Route>
+    </Routes>
   );
-}
+};
+
+export default App;
