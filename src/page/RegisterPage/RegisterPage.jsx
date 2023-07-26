8896a51d307f84b8b4422e4bf75c7bdf7a335a67
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-26 15:45:28 +0300

    Create RegisterPage.jsx

---
+++ src/page/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,13 @@
+import { RegisterForm } from '../../components/RegisterForm/RegisterForm';
+
+
+const RegisterPage = () => {
+    return(
+        <div>
+            <h1>Register your profile</h1>
+            <RegisterForm />
+        </div>
+    )
+};
+
+export default RegisterPage;
