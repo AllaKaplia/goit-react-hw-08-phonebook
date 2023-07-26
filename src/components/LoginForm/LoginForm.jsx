61bf983dda96d02f8b34e3109431db259d6f540f
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-26 23:46:07 +0300

    Update LoginForm.jsx

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,9 +1,31 @@
-const { Formik, Form, Field } = require("formik")
+import { Formik, Form, Field } from 'formik';
+import * as yup from 'yup';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectorIsLoggedIn } from 'redux/auth/authSelectors';
+import { logIn } from 'redux/auth/authOperations';
 
+
+const schema = yup.object().shape({
+    email: yup.string().matches(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'E-mail is invalid').email().required('E-mail is required'),
+    password: yup.string().length(10, 'Enter 10 characters').required('Password is required')
+});
+
+const initialValue = {
+    email: '',
+    password: '',
+}
 
 const LoginForm = ()  => {
+    const dispatch = useDispatch();
+
+    const handleSubmit = () => {
+        dispatch(logIn(
+            
+        ))
+    }
+
     return (
-        <Formik>
+        <Formik initialValues={initialValue} validationSchema={schema} >
             <Form>
                 <label>
                     Enter yor email
