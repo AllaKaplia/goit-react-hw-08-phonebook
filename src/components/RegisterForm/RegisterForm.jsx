81a6583e6bbc9a116922cc6275e50ae65c58d059
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-27 02:19:59 +0300

    Update RegisterForm.jsx

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,21 +1,49 @@
-const { Formik, Form, Field } = require("formik")
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useDispatch } from "react-redux";
+import { register } from "redux/auth/authOperations";
+import * as yup from 'yup';
+
+const initialValues = {
+    name: '',
+    email: '',
+    password: '',
+}
+
+const schema = yup.object().shape({
+    name: yup
+    .string()
+    .matches(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/, 'Name is invalid')
+    .required('Name is required'),
+    email: yup.string().matches(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'E-mail is invalid').email().required('E-mail is required'),
+    password: yup.string().length(10, 'Enter 10 symbols').required('Password is required')
+});
 
 
 const RegisterForm = () => {
+    const dispatch = useDispatch();
+
+    const handleSubmit = (values, { resetForm }) => {
+        dispatch(register(values));
+        resetForm();
+    }
+
     return (
-        <Formik>
+        <Formik initialValues={initialValues} validationSchema={schema} onSubmit={handleSubmit} >
             <Form autoComplete="off">
                 <label>
-                    Username:
+                    <span>Username:</span>
                     <Field type="text" name="name" placeholder="Enter username" />
+                    <ErrorMessage name="name" component="div" />
                 </label>
                 <label>
-                    Your e-mail address:
+                    <span>Your e-mail address:</span>
                     <Field type="email" name="email" placeholder="Enter e-mail address" />
+                    <ErrorMessage name="email" component="div" />
                 </label>
                 <label>
-                    Enter your password:
+                    <span>Enter your password:</span>
                     <Field type="password" name="password" placeholder="Enter password" />
+                    <ErrorMessage name="password" component="div" />
                 </label>
                 <button type="submit" >
                     Register
