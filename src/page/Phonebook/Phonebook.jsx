c97b2167eaea8c8636637ec47c4f17541dc097e2
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-27 15:40:45 +0300

    Update Phonebook.jsx

--- src/page/Phonebook/Phonebook.jsx
+++ src/page/Phonebook/Phonebook.jsx
@@ -1,16 +1,18 @@
 import ContactList from 'components/ContactList';
 import FilterContacts from 'components/FilterContacts';
 import FormContact from 'components/FormContact';
+import { TitleForm } from 'components/RegisterForm/RegisterForm.styled';
+import { TitleH2 } from './Phonebook.styled';
 
 
 const Phonebook = () => {
     return (
         <div>
-            <h1>Phonebook</h1>
-            <h2>Enter the data of the new contact</h2>
+            <TitleForm>Phonebook</TitleForm>
+            <TitleH2>Enter the data of the new contact</TitleH2>
             <FormContact />
-            <h2>Contacts list</h2>
-            <h3>You can use the search</h3> <FilterContacts />
+            <TitleH2>Contacts list</TitleH2>
+            <FilterContacts />
             <ContactList />
         </div>
     )
