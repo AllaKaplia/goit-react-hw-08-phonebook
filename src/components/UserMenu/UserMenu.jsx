cb7dddd8373925b1fd7ede97c52f2f40c0128592
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-27 15:40:22 +0300

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,6 +1,9 @@
 import { useAuth } from "hooks";
 import { useDispatch } from "react-redux";
 import { logOut } from "redux/auth/authOperations";
+import { BoxMenu, HelloUser } from "./UserMenu.styled";
+import Button from '@mui/material/Button';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 
 
 const UserMenu = () => {
@@ -9,10 +12,10 @@ const UserMenu = () => {
     const { user } = useAuth();
 
     return (
-        <div>
-            <p>Congratulations, {user.name}</p>
-            <button type="button" onClick={() => dispatch(logOut())}>Log Out</button>
-        </div>
+        <BoxMenu>
+            <HelloUser>Congratulations, {user.name} <EmojiEmotionsIcon /> </HelloUser>
+            <Button variant="contained" color="success" type="button" onClick={() => dispatch(logOut())}>Log Out</Button>
+        </BoxMenu>
     )
 }
 
