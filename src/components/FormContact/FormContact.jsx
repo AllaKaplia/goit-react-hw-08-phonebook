63f364dbead813c60da133c90326b6061cfe3dee
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-27 15:39:47 +0300

    Update FormContact.jsx

--- src/components/FormContact/FormContact.jsx
+++ src/components/FormContact/FormContact.jsx
@@ -5,8 +5,10 @@ import toast from 'react-hot-toast';
 import * as yup from 'yup';
 import { addContact } from '../../redux/contactsOperations';
 import { selectVisibleContacts } from '../../redux/selectors';
-import { Input, ButtonAdd, LabelForm, FormBox, BigBoxForm } from './FormContact.styled';
+import { Input, LabelForm } from './FormContact.styled';
 import { PiAddressBookLight } from 'react-icons/pi';
+import { BoxBig, ContainerForm, LabelText } from 'components/RegisterForm/RegisterForm.styled';
+import Button from '@mui/material/Button';
 
 const schema = yup.object().shape({
   name: yup
@@ -57,36 +59,38 @@ const FormContact = () => {
   };
 
   return (
-    <Formik initialValues={initialValue} validationSchema={schema} onSubmit={handleFormSubmit}>
-      <Form autoComplete='off'>
-        <BigBoxForm>
-        <FormBox>
-          <LabelForm>
-            The name of the new contact
-            <Input
-              type='text'
-              name='name'
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </LabelForm>
-          <LabelForm>
-            Phone number
-            <Input
-              type='tel'
-              name='number'
-              title='Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
-              required
-            />
-          </LabelForm>
-        </FormBox>
-        <ButtonAdd type='submit'>
-            <PiAddressBookLight />
-            Add contact
-          </ButtonAdd>
-        </BigBoxForm>
-      </Form>
-    </Formik>
+    <ContainerForm>
+      <Formik initialValues={initialValue} validationSchema={schema} onSubmit={handleFormSubmit}>
+        <Form autoComplete='off'>
+            <BoxBig>
+              <LabelForm>
+                <LabelText>The name of the new contact</LabelText>
+                <Input
+                  type='text'
+                  name='name'
+                  placeholder="Enter name new contact" 
+                  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                  required
+                />
+              </LabelForm>
+              <LabelForm>
+                <LabelText>Phone number</LabelText>
+                <Input
+                  type='tel'
+                  name='number'
+                  placeholder="Enter phone number" 
+                  title='Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+                  required
+                />
+              </LabelForm>
+              <Button variant="contained" color="success" type="submit" >
+                <PiAddressBookLight size={30} />
+                Add contact
+              </Button>
+            </BoxBig>
+        </Form>
+      </Formik>
+    </ContainerForm>
   );
 };
 
