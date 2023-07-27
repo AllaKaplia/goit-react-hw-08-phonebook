a7bcbeda2f560f9a9276b64e2252d403c1149e00
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-27 15:40:25 +0300

    Create UserMenu.styled.jsx

---
+++ src/components/UserMenu/UserMenu.styled.jsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+export const BoxMenu = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: flex-end;
+    gap: 15px;
+`;
+
+export const HelloUser = styled.p`
+    display: flex;
+    gap: 10px;
+    font-size: 22px;
+    font-weight: 700;
+    color: #fff;
+    align-items: center;
+`;
