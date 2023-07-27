b92f68b57a31387fc18ee11bd2296bd2664001ec
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-27 15:40:50 +0300

    Create Phonebook.styled.jsx

---
+++ src/page/Phonebook/Phonebook.styled.jsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const TitleH2 = styled.h2`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    text-align: center;
+    font-weight: 600;
+    font-size: 30px;
+    color: #0b9448;
+    margin-bottom: 20px;
+`;
