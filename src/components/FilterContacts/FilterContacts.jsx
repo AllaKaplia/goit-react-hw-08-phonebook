b51ffe41c7923a9bfb3277d0cf2b258091803471
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-27 15:39:40 +0300

    Update FilterContacts.jsx

--- src/components/FilterContacts/FilterContacts.jsx
+++ src/components/FilterContacts/FilterContacts.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { LabelFilter, InputFilter } from "./FilterContacts.styled";
+import { LabelFilter, InputFilter, LabelSpan } from "./FilterContacts.styled";
 import { useDispatch, useSelector } from "react-redux";
 import { selectFilters } from "redux/selectors";
 import { changedFilter } from "redux/filterSlice";
+import PersonSearchIcon from '@mui/icons-material/PersonSearch';
 
 const FilterContacts = () => {
   const dispatch = useDispatch();
@@ -10,11 +11,12 @@ const FilterContacts = () => {
 
   return (
     <LabelFilter>
-      Filter
+      <LabelSpan> <PersonSearchIcon /> Searching contact</LabelSpan>
       <InputFilter
         type="text"
         value={filter}
         name="filter"
+        placeholder="Enter name contact"
         onChange={evt => dispatch(changedFilter(evt.target.value.trim()))} 
       />
     </LabelFilter>
