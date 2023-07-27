837ee5ec148bfbc84222711b281a39f88b8bad32
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-27 15:40:34 +0300

    Update Home.jsx

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,6 +1,6 @@
 import photoHero from '../../img/phone-number.png';
 import HomeIcon from '@mui/icons-material/Home';
-import { BoxHome, HeroHome, MainImg, TitleHome, Text } from './Home.styled';
+import { BoxHome, HeroHome, MainImg, TitleHome, Text, List } from './Home.styled';
 import LoginIcon from '@mui/icons-material/Login';
 import HowToRegIcon from '@mui/icons-material/HowToReg';
 import AssuredWorkloadIcon from '@mui/icons-material/AssuredWorkload';
@@ -15,14 +15,14 @@ const Home = () => {
             <HeroHome>
                 <MainImg src={photoHero} alt="Phone book" width={400} />
                 <p><Text>This place is specifically created for storing your contacts that no longer fit in your notebook. You can create a convenient list here with no limitations on the number. How to use the application:</Text>
-                    <ul>
+                    <List>
                         <li>1. Register in the system.  <HowToRegIcon /> </li>
                         <li>2. Log in / Authenticate. <LoginIcon /> </li>
                         <li>3. Go to the 'Contacts' page. <AssuredWorkloadIcon /> </li>
                         <li>4. Create your list by entering the name and phone number. <EditNoteIcon /> </li>
                         <li>5. You can delete unnecessary contacts. <PersonRemoveIcon /> </li>
                         <li>6. Use the form for convenient search. <PersonSearchIcon /> </li>
-                    </ul>
+                    </List>
                     <Text>And remember: this is your personal contact list. Only you have access to it. We prioritize the security of your personal data!</Text>
                 </p>
             </HeroHome>
