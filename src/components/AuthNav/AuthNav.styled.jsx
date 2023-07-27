90b2c31509ab27065a7b0373aa57a1b0dc02fb6a
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-27 15:56:58 +0300

    Update AuthNav.styled.jsx

--- src/components/AuthNav/AuthNav.styled.jsx
+++ src/components/AuthNav/AuthNav.styled.jsx
@@ -1,6 +1,28 @@
 import styled from '@emotion/styled';
+import { NavLink } from "react-router-dom";
 
 export const ContainerAuth = styled.div`
     display: flex;
     gap: 25px;
+`;
+
+export const StyleNav = styled(NavLink)`
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    font-size: 25px;
+    color: #fff;
+    font-weight: 700;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 8px 16px;
+    transition: background-color 0.2s, color 0.2s, box-shadow 0.2s;
+        &:hover {
+            background-color: #c4f8af;
+            color: #0b9448;
+        };
+        &:focus {
+            outline: none;
+            box-shadow: 0 0 5px #ccc;
+        };
 `;
