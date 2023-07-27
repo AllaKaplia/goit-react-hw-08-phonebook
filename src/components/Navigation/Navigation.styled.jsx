082b66bfb9dcbd7885f082116daf247c2c91cd18
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-27 04:44:08 +0300

    Create Navigation.styled.jsx

---
+++ src/components/Navigation/Navigation.styled.jsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+import { NavLink } from "react-router-dom";
+
+export const Navigation = styled(NavLink)`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    font-size: 25px;
+    color: #03ad03;
+    font-weight: 700;
+`;
+
