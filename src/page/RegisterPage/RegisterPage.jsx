6aabb3bddfc1cc6b8e5428ec72db35163af5b0d3
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-27 15:40:53 +0300

    Update RegisterPage.jsx

--- src/page/RegisterPage/RegisterPage.jsx
+++ src/page/RegisterPage/RegisterPage.jsx
@@ -1,12 +1,14 @@
+import { TitleForm } from 'components/RegisterForm/RegisterForm.styled';
+import { BoxHome } from 'page/Home/Home.styled';
 import RegisterForm from '../../components/RegisterForm/RegisterForm';
 
 
 const RegisterPage = () => {
     return(
-        <div>
-            <h1>Register your profile</h1>
+        <BoxHome>
+            <TitleForm>Register your profile</TitleForm>
             <RegisterForm />
-        </div>
+        </BoxHome>
     )
 };
 
