29751682e93bbd8c6624803ec82ee143ccb44c73
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-27 15:40:40 +0300

    Create Login.styled.jsx

---
+++ src/page/Login/Login.styled.jsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const LoginTitle = styled.h1`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    text-align: center;
+    font-weight: 700;
+    font-size: 40px;
+    color: #0b9448;
+    margin-bottom: 20px;
+    margin-top: 10px;
+`;
