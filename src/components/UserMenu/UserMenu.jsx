1ca716fb88177d6feadf04049ce17b248532f327
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-27 20:19:56 +0300

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -4,17 +4,39 @@ import { logOut } from "redux/auth/authOperations";
 import { BoxMenu, HelloUser } from "./UserMenu.styled";
 import Button from '@mui/material/Button';
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
+import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
+import { useState } from "react";
 
 
 const UserMenu = () => {
     const dispatch = useDispatch();
-
     const { user } = useAuth();
+    const [isHovered, setIsHovered] = useState(false);
 
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
     return (
         <BoxMenu>
             <HelloUser>Congratulations, {user.name} <EmojiEmotionsIcon /> </HelloUser>
-            <Button variant="contained" color="success" type="button" onClick={() => dispatch(logOut())}>Log Out</Button>
+            <Button
+                variant="contained"
+                color="success"
+                type="button"
+                onClick={() => dispatch(logOut())}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                onFocus={handleMouseEnter}
+                onBlur={handleMouseLeave}
+            >
+                {isHovered ? <SentimentVeryDissatisfiedIcon /> : <SentimentSatisfiedAltIcon />}
+                Log Out
+            </Button>
         </BoxMenu>
     )
 }
